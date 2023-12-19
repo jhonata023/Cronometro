10cd71086e3da18b9c3da696bdc37d11aac5c3a3
Medeiros <137403955+jhonata023@users.noreply.github.com>
2023-12-19 20:32:04 -0300

    Fazendo funcionar
    Funções de pausar, continuar e reiniciar

--- main.js
+++ main.js
@@ -7,37 +7,43 @@ const btnPausar = document.querySelector('#pausar');
 const btnContinuar = document.querySelector('#continuar');
 const btnRedefinir = document.querySelector('#redefinir');
 
+let intervalo
+let pause = false;
 let sec = 0;
 let min = 0;
 let mil = 0;
 
+
 btnIniciar.addEventListener('click', () => {
-    setInterval(() => {
-        mil += 1
-        milesimos.innerText = mil;
-
-        if (mil == 100) {
-            mil = 0;
-            sec += 1;
-            if (sec < 10) {
-                segundos.innerText = `0${String(sec)}`;
-            } else {
-                segundos.innerText = sec;
+    intervalo = setInterval(() => {
+        if (!pause) {
+            mil += 1
+            milesimos.innerText = mil;
+    
+            if (mil == 100) {
+                mil = 0;
+                sec += 1;
+                if (sec < 10) {
+                    segundos.innerText = `0${String(sec)}`;
+                } else {
+                    segundos.innerText = sec;
+                }
+                milesimos.innerText = 0;
             }
-            milesimos.innerText = 0;
-        }
 
-        if (sec == 60) {
-            sec = 0;
-            min += 1;
-            if (min < 10) {
-                minutos.innerText = `0${String(min)}`;
-            } else {
-                minutos.innerText = min;
+            if (sec == 60) {
+                sec = 0;
+                min += 1;
+                if (min < 10) {
+                    minutos.innerText = `0${String(min)}`;
+                } else {
+                    minutos.innerText = min;
+                }
+                segundos.innerText = 0;
             }
-            segundos.innerText = 0;
         }
     }, 10)
+    
 
     btnPausar.classList.add('btn');
     btnPausar.classList.remove('none');
@@ -53,8 +59,7 @@ btnPausar.addEventListener('click', () => {
     btnContinuar.classList.add('btn');
     btnContinuar.classList.remove('none');
 
-    const secTravado = segundos.innerText;
-    segundos.innerText = secTravado;
+    pause = true;
 })
 
 btnContinuar.addEventListener('click', () => {
@@ -62,8 +67,29 @@ btnContinuar.addEventListener('click', () => {
     btnContinuar.classList.remove('btn');
     btnPausar.classList.add('btn');
     btnPausar.classList.remove('none');
+
+    pause = false;
 })
 
 btnRedefinir.addEventListener('click', () => {
-    location.reload();
+    // location.reload();
+    clearInterval(intervalo)
+
+    pause = false;
+    sec = 0;
+    min = 0;
+    mil = 0;
+
+    milesimos.innerText = '0'+`${mil}`;
+    segundos.innerText = '0'+`${sec}`;
+    minutos.innerText = '0'+`${min}`;
+
+    btnPausar.classList.add('none');
+    btnPausar.classList.remove('btn');
+    btnRedefinir.classList.add('none');
+    btnRedefinir.classList.remove('btn');
+    btnContinuar.classList.add('none');
+    btnContinuar.classList.remove('btn');
+    btnIniciar.classList.add('btn');
+    btnIniciar.classList.remove('none');
 })
